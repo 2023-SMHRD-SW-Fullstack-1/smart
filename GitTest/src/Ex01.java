5b4d9043ff4bbfa6943597d5cb42de63c6f93785
smhrd <smhrd@DESKTOP-ITSTFDF>
2023-04-14 16:37:21 +0900

    222

--- GitTest/src/Ex01.java
+++ GitTest/src/Ex01.java
@@ -5,7 +5,7 @@ public class Ex01 {
 		System.out.println("팀장님 첫번째 커밋");
         System.out.println("팀원1 두번째 커밋");
         System.out.println("세번째 커밋");
-        
+        System.out.println("121112");
 	}
 
 }
