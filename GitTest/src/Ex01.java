adf01da235d7be567a5c232f91308bf66551fe8e
smhrd <smhrd@DESKTOP-ITSTFDF>
2023-04-14 16:22:20 +0900

    세번째 커밋

--- GitTest/src/Ex01.java
+++ GitTest/src/Ex01.java
@@ -4,6 +4,8 @@ public class Ex01 {
 	public static void main(String[] args) {
 		System.out.println("팀장님 첫번째 커밋");
         System.out.println("팀원1 두번째 커밋");
+        System.out.println("세번째 커밋");
+        
 	}
 
 }
